749ce3499ffc36917ee96166fe8366c08f5bd7dc
William <wormy784@byu.edu>
2026-01-14 12:44:43 -0700

    Setup memory tracking and implemented pawn move up 1 and move up 2 on start of turn.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,8 +11,9 @@ package chess;
  */
 public class ChessBoard {
 
+    ChessPiece[][] squares = new ChessPiece[8][8];
     public ChessBoard() {
-        
+
     }
 
     /**
@@ -19,7 +23,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        squares[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
@@ -30,7 +34,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
@@ -39,5 +43,19 @@ public class ChessBoard {
      */
     public void resetBoard() {
         throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,9 @@
 package chess;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,7 +13,26 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
@@ -29,14 +51,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +69,61 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        //create moves collection to return later
+        ChessPiece piece = board.getPiece(myPosition);
+        Collection<ChessMove> moves = new ArrayList<ChessMove>();
+        // pawn move set
+        if (getPieceType() == PieceType.PAWN) {
+            int direction = pawn_direction();
+            int row = myPosition.getRow();
+            int column = myPosition.getColumn();
+            int nextRow = row + direction;
+
+            // MOVE 1: create destination square one space ahead, if it is empty, the pawn can move there
+            ChessPosition destination_square = new ChessPosition(nextRow, column);
+//          // ask if there is a piece on that square
+            if (board.getPiece(destination_square) == null) {
+                // if no square, the make a chess move
+                ChessMove pawn_move_one_forward = new ChessMove(myPosition, destination_square, null);
+                // add the move
+                moves.add(pawn_move_one_forward);
+            }
+
+            // MOVE 2: if it is pawns first move and two spaces ahead are clear it can move there
+            // if white
+            if (direction == 1) {
+                if (myPosition.getRow() == 2) {
+                    ChessPosition destination_square2 = new ChessPosition(4,column);
+                    if (board.getPiece(destination_square2) == null && board.getPiece(destination_square) == null) {
+                        ChessMove pawn_move_two_forward = new ChessMove(myPosition, destination_square2, null);
+                        moves.add(pawn_move_two_forward);
+                    }
+                }
+            }
+            // if black
+            if (direction == -1) {
+                if (myPosition.getRow() == 7) {
+                    ChessPosition destination_square2 = new ChessPosition(5,column);
+                    if (board.getPiece(destination_square2) == null && board.getPiece(destination_square) == null) {
+                        ChessMove pawn_move_two_forward = new ChessMove(myPosition, destination_square2, null);
+                        moves.add(pawn_move_two_forward);
+                    }
+                }
+            }
+
+        }
+
+        return moves;
+
+    }
+
+    //pawn direction helper function
+    private int pawn_direction() {
+        int direction = 0;
+        if (getTeamColor() == ChessGame.TeamColor.WHITE) {
+            direction = 1; }
+        if (getTeamColor() == ChessGame.TeamColor.BLACK) {
+            direction = -1; }
+        return direction;
     }
 }
