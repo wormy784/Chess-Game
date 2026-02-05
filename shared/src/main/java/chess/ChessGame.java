5cb445398be25c765a37f37c74f2374b8d68624f
William <wormy784@byu.edu>
2026-02-04 20:55:49 -0700

    Promotion piece pawn handled

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -84,17 +84,27 @@ public class ChessGame {
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
         var piece = board.getPiece(move.getStartPosition());
-        if (piece == null) {
-            throw new InvalidMoveException("That's not a real move!");
-        }
-        if (piece.getTeamColor() != turn) {
-            throw new InvalidMoveException("That's not a real move!");
-
-        }
+        var target = board.getPiece(move.getEndPosition());
         ChessPosition start = move.getStartPosition();
         ChessPosition end = move.getEndPosition();
+
+        // 1) throw if no piece
+        if (piece == null) {
+            throw new InvalidMoveException("There's no piece there!");
+        }
+        // 2) throw if other team
+        if (piece.getTeamColor() != turn) {
+            throw new InvalidMoveException("That's the wrong team!");
+
+        }
+        // 3) check if move is legal move for that piece
+
+        Collection<ChessMove> moves = validMoves(move.getStartPosition());
+        if (moves == null || !moves.contains(move)) {
+            throw new InvalidMoveException("That's not a real move!");
+        }
+
         // cant take own piece
-        var target = board.getPiece(move.getEndPosition());
         if (target != null) {
             if (piece.getTeamColor() == target.getTeamColor()) {
                 throw new InvalidMoveException("You can't take your own piece.");
@@ -104,9 +114,18 @@ public class ChessGame {
         board.addPiece(start, null);
         //place piece at end pos
         board.addPiece(end, piece);
+        // if piece is a pawn and on right squares, it can promote
+        if (piece.getPieceType() == ChessPiece.PieceType.PAWN) {
+            if ((end.getRow() == 1 || end.getRow() == 8)) {
+                ChessPiece.PieceType promotion = move.getPromotionPiece();
+                ChessPiece promoted_piece = new ChessPiece(piece.getTeamColor(), promotion);
+                board.addPiece(end, promoted_piece);
+            }
+        }
         // cant move if puts king in check
         if (isInCheck(turn)) {
-            board.addPiece(start, null);
+            board.addPiece(start, piece);
+            board.addPiece(end, target);
             throw new InvalidMoveException("That puts the king in check!");
         }
 
