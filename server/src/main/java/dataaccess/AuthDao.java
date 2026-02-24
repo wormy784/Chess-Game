e66cbaacb69ffdac12f783e4331a7e153d25829d
William <wormy784@byu.edu>
2026-02-24 11:45:36 -0700

    AuthDao createauth, getauth, and deleteauth completed.

--- server/src/main/java/dataaccess/AuthDao.java
+++ server/src/main/java/dataaccess/AuthDao.java
@@ -1,4 +1,24 @@
 package dataaccess;
-
+import java.util.UUID;
+import java.util.HashMap;
+import model.AuthData;
 public class AuthDao {
+    //hashmap to store auth data
+    private final HashMap<String, AuthData> authTokens = new HashMap<>();
+
+    // create authentication token when user registers or logs in
+    void createAuth(AuthData auth) throws DataAccessException {
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    // retrieve auth data by token
+    AuthData getAuth(String authToken) throws DataAccessException {
+        return authTokens.get(authToken);
+    }
+
+    // remove auth token specifically for logout
+    void deleteAuth(String authToken) throws DataAccessException {
+        authTokens.remove(authToken);
+    }
+
 }
