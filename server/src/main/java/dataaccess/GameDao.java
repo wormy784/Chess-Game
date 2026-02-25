fd27c508a3d5616b16d10cafb326f37a56c24bfc
William <wormy784@byu.edu>
2026-02-24 19:04:30 -0700

    Finished GameDao including createGame, get game, listgames, and update games

--- server/src/main/java/dataaccess/GameDao.java
+++ server/src/main/java/dataaccess/GameDao.java
@@ -2,17 +2,35 @@ package dataaccess;
 
 import model.GameData;
 
-class GameDao {
-    void createGame(GameData g) {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
+public class GameDao {
+    private int nextGameID = 1;
+    // hashmap to store games
+    private final HashMap<Integer, GameData> games = new HashMap<>();
+
+    void createGame(GameData g) throws DataAccessException {
+        GameData newGame = new GameData(nextGameID, g.whiteUsername(), g.blackUsername(),
+                g.gameName(), g.game());
+
+        // Add to hashmap
+        games.put(newGame.gameID(), newGame);
+        //increment gameid
+        nextGameID++;
     }
 
-    GameData getGame(int gameId) {
-        return null;
+    GameData getGame(int gameId) throws DataAccessException {
+        return games.get(gameId);
     }
 
-    void updateGame(GameData g) {
+    Collection<GameData> listGames() throws DataAccessException {
+        return games.values();
+    }
 
+    void updateGame(GameData g) throws DataAccessException {
+        games.put(g.gameID(), g);
     }
 
 }
