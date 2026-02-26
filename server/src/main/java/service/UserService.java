66c707b36efdf56f87fcd0a5ecc299ddf0844da3
William <wormy784@byu.edu>
2026-02-26 13:14:11 -0700

    Logout complete!

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -8,11 +8,29 @@ public class UserService {
     private AuthDao authDao;
 
     // constructor
+    public UserService(UserDao userDao, AuthDao authDao) {
+        this.userDao = userDao;
+        this.authDao = authDao;
+    }
 
     // register
+    public AuthData register(String username, String password, String email) throws DataAccessException {
 
+    }
     //login
+    public AuthData login(String username, String password) throws DataAccessException {
+
+    }
 
     // logout
-
+    public void logout(String authToken) throws DataAccessException {
+        // check if the token exists
+        AuthData auth = authDao.getAuth(authToken);
+        // if null then error unauthorized exception
+        if (auth == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        // if authdata exists, remove it
+        authDao.deleteAuth(authToken);
+    }
 }
