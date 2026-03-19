f7a879bbca1c98e7619ddb59ecad84a53c0d582e
William <wormy784@byu.edu>
2026-03-19 13:37:29 -0600

    List games, create games, logout, login tests done.

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,7 +1,6 @@
 package client;
 
-import model.AuthData;
-import model.RegisterRequest;
+import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
 
@@ -34,7 +33,9 @@ public class ServerFacadeTests {
 
     @Test
     public void registerPosTest() throws Exception {
+        //request
         RegisterRequest request = new RegisterRequest("username", "password123", "email@gmail.com");
+        // register that request
         AuthData testResult = facade.register(request);
         Assertions.assertNotNull(testResult);
     }
@@ -46,4 +47,106 @@ public class ServerFacadeTests {
         new RegisterRequest("username", "password123", "email@gmail.com");
         Assertions.assertThrows(Exception.class, () -> facade.register(request));
     }
+
+    @Test
+    public void loginPosTest() throws Exception {
+        //register request
+        RegisterRequest request = new RegisterRequest("username", "password123", "email@gmail.com");
+        // register request
+        facade.register(request);
+        // login request
+        LoginRequest request2 = new LoginRequest("username", "password123");
+        // login with username
+        AuthData loginTest = facade.login(request2);
+        Assertions.assertNotNull(loginTest);
+    }
+
+    @Test
+    public void loginNegTest() throws Exception {
+        //register request
+        RegisterRequest request = new RegisterRequest("username", "password123", "email@gmail.com");
+        // register request
+        facade.register(request);
+        // login request with fake username
+        LoginRequest request2 = new LoginRequest("usernamefake", "password123");
+        Assertions.assertThrows(Exception.class, () -> facade.login(request2));
+    }
+
+    @Test
+    public void logoutPosTest() throws Exception {
+        //register request
+        RegisterRequest request = new RegisterRequest("username", "password123", "email@gmail.com");
+        // register request
+        facade.register(request);
+        // login request
+        LoginRequest request2 = new LoginRequest("username", "password123");
+        // login with username
+        AuthData authToken = facade.login(request2);
+        // try logging out, make sure nothing throws bro
+        Assertions.assertDoesNotThrow(() -> facade.logout(authToken));
+    }
+
+    @Test
+    public void logoutNegTest() throws Exception {
+        // fake token
+        AuthData fakeToken = new AuthData("token", "jimmyjohn");
+        // try logging out, make sure nothing throws bro
+        Assertions.assertThrows(Exception.class, () -> facade.logout(fakeToken));
+    }
+
+    @Test
+    public void createGamePosTest() throws Exception {
+        //register request
+        RegisterRequest request = new RegisterRequest("username", "password123", "email@gmail.com");
+        // register request
+        facade.register(request);
+        // login request
+        LoginRequest request2 = new LoginRequest("username", "password123");
+        // login with username
+        AuthData authToken = facade.login(request2);
+        CreateGameRequest gameName = new CreateGameRequest("FortniteBattlePass");
+        Assertions.assertNotNull(facade.createGame(gameName, authToken));
+    }
+
+    @Test
+    public void createGameNegTest() throws Exception {
+        // fake token
+        AuthData fakeToken = new AuthData("token", "jimmyjohn");
+        // try creating the game, make sure it throws bro since its a fake token
+        CreateGameRequest gameName = new CreateGameRequest("FortniteBattlePass");
+        Assertions.assertThrows(Exception.class, () -> facade.createGame(gameName, fakeToken));
+    }
+
+    @Test
+    public void listGamesPosTest() throws Exception {
+        //register request
+        RegisterRequest request = new RegisterRequest("username", "password123", "email@gmail.com");
+        // register request
+        facade.register(request);
+        // login request
+        LoginRequest request2 = new LoginRequest("username", "password123");
+        // login with username
+        AuthData authToken = facade.login(request2);
+        CreateGameRequest gameName = new CreateGameRequest("FortniteBattlePass");
+        facade.createGame(gameName, authToken);
+        CreateGameRequest gameName2 = new CreateGameRequest("Minecraft");
+        facade.createGame(gameName2, authToken);
+        ListGamesResult games = facade.listGames(authToken);
+        Assertions.assertEquals(2, games.games().size());
+    }
+
+    @Test
+    public void listGamesNegTest() throws Exception {
+        // fake token and assert that it actually trhows bro
+        AuthData fakeToken = new AuthData("token", "jimmyjohn");
+        Assertions.assertThrows(Exception.class, () -> facade.listGames(fakeToken));
+    }
+
+
+
+
+
+
+
+
 }
