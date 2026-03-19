10f9a76307e97e821756dd0187cd45f3b751d4c1
William <wormy784@byu.edu>
2026-03-19 00:16:31 -0600

    Server Facade tests initialized

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,17 +2,26 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.handler.ClearHandler;
+import service.ClearService;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+    }
+
+    @BeforeEach
+    void clearDatabase() throws Exception {
+        ClearService
     }
 
     @AfterAll
