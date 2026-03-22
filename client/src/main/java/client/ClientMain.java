ecfec913eb7574fab3c3b94dc39629f564894d16
William <wormy784@byu.edu>
2026-03-21 18:33:59 -0600

    Client main done

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,12 @@
 package client;
 
 import chess.*;
-
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        //facade
+        ServerFacade facade = new ServerFacade(8080);
+        // create repl with facade
+        Repl repl = new Repl(facade);
+        repl.run();
     }
 }
