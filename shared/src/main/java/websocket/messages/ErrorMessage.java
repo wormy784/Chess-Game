a88976939eff20b68716c0b91a42e8f46a1fe63a
William <wormy784@byu.edu>
2026-04-08 11:22:49 -0600

    Finished Error message class.

--- shared/src/main/java/websocket/messages/ErrorMessage.java
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -1,4 +1,9 @@
 package websocket.messages;
 
-public class ErrorMessage {
+public class ErrorMessage extends ServerMessage {
+    String errorMessage;
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessage.ServerMessageType.ERROR);
+        this.errorMessage = errorMessage;
+    }
 }
