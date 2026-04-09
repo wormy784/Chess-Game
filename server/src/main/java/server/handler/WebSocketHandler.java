0ec98cf84248ee528668119e6f9faf15ed5b0688
William <wormy784@byu.edu>
2026-04-09 17:49:51 -0600

    Handle Make moves done!

--- server/src/main/java/server/handler/WebSocketHandler.java
+++ server/src/main/java/server/handler/WebSocketHandler.java
@@ -1,6 +1,8 @@
 package server.handler;
 
+import chess.ChessGame;
 import chess.ChessMove;
+import chess.InvalidMoveException;
 import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import dataaccess.IAuthDao;
@@ -8,6 +10,7 @@ import io.javalin.websocket.WsCloseContext;
 import io.javalin.websocket.WsConnectContext;
 import io.javalin.websocket.WsMessageContext;
 import service.GameService;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 
 import java.util.HashSet;
@@ -38,11 +41,15 @@ public class WebSocketHandler {
         UserGameCommand command = gson.fromJson(jsonString, UserGameCommand.class);
 
 
+
         // switch on commandType and route to other methods
         switch (command.getCommandType()) {
             case CONNECT: handleConnect(context, command);
             break;
-            case MAKE_MOVE: break;
+            case MAKE_MOVE:
+                MakeMoveCommand makeMoveCommand = gson.fromJson(jsonString, MakeMoveCommand.class);
+                handleMakeMove(context, makeMoveCommand);
+            break;
             case LEAVE: break;
             case RESIGN: break;
         }
@@ -75,23 +82,82 @@ public class WebSocketHandler {
                 var notifMessage = new NotificationMessage(username + " joined the game");
                 var notifJson = gson.toJson(notifMessage);
                 session.send(notifJson);
-
             }
         } catch (DataAccessException e) {
             context.send(gson.toJson(new ErrorMessage("Error: " + e.getMessage())));
         }
     }
 
-    private void handleMakeMove(WsMessageContext context, UserGameCommand command) {
-        // verify move
+    private void handleMakeMove(WsMessageContext context, MakeMoveCommand command) {
+            try {
+                // verify move
+                var updatedGame = gameService.makeMove(command.getAuthToken(), command.getGameID(), command.getMove());
+                // update game in database
+                var message = new LoadGameMessage(updatedGame);
+                var json = gson.toJson(message);
+                // username dealio
+                var username = authDao.getAuth(command.getAuthToken()).username();
+                // load game go to all sessions
+                for (WsContext session: gameSessions.get(command.getGameID())) {
+                    session.send(json);
+                }
+                // send notification to other clients
+                for (WsContext session : gameSessions.get(command.getGameID())) {
 
-        // update game in database
+                    var notifMessage = new NotificationMessage(username + "Move Succcess");
+                    var notifJson = gson.toJson(notifMessage);
+                    // server send Notification message ot all other clients about what move was made
+                    if (session != context) {
+                        session.send(notifJson);
+                    }
+                }
+                // if move is a check, checkmate or stalemate, server send notification message to all clients
+                // white
+                if (updatedGame.game().isInCheckmate(ChessGame.TeamColor.WHITE)) {
+                    var checkMessage = gson.toJson(new NotificationMessage("White is in checkmate!"));
+                    for (WsContext s : gameSessions.get(command.getGameID())) {
+                        s.send(checkMessage);
+                    }
+                }
+                // white stalemate
+                else if (updatedGame.game().isInStalemate(ChessGame.TeamColor.WHITE)) {
+                    var checkMessage = gson.toJson(new NotificationMessage("White is in stalemate!"));
+                    for (WsContext s : gameSessions.get(command.getGameID())) {
+                        s.send(checkMessage);
+                    }
+                }
+                // white in check
+                else if (updatedGame.game().isInCheck(ChessGame.TeamColor.WHITE)) {
+                    var checkMessage = gson.toJson(new NotificationMessage("White is in check!"));
+                    for (WsContext s : gameSessions.get(command.getGameID())) {
+                        s.send(checkMessage);
+                    }
+                }
+                //black
+                if (updatedGame.game().isInCheckmate(ChessGame.TeamColor.BLACK)) {
+                    var checkMessage = gson.toJson(new NotificationMessage("BLACK is in checkmate!"));
+                    for (WsContext s : gameSessions.get(command.getGameID())) {
+                        s.send(checkMessage);
+                    }
+                }
+                // black stalemate
+                else if (updatedGame.game().isInStalemate(ChessGame.TeamColor.BLACK)) {
+                    var checkMessage = gson.toJson(new NotificationMessage("Black is in stalemate!"));
+                    for (WsContext s : gameSessions.get(command.getGameID())) {
+                        s.send(checkMessage);
+                    }
+                }
+                // black check
+                else if (updatedGame.game().isInCheck(ChessGame.TeamColor.BLACK)) {
+                    var checkMessage = gson.toJson(new NotificationMessage("BLACK is in check!"));
+                    for (WsContext s : gameSessions.get(command.getGameID())) {
+                        s.send(checkMessage);
+                    }
+                }
 
-        // server send LOAD_GAME message to all clients
-
-        // server send Notification message ot all other clients about what move was made
-
-        // if move is a check, checkmate or stalemate, server send notification message to all clients
+            } catch (DataAccessException | InvalidMoveException e) {
+                context.send(gson.toJson(new ErrorMessage("Error: " + e.getMessage())));
+            }
 
     }
     private void onClose(WsCloseContext context) {
