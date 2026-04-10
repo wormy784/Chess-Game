a8d77b7f6d1f2097d498f7e85db53cdce45e8c13
William <wormy784@byu.edu>
2026-04-09 19:00:03 -0600

    WebSocketFacade completed.

---
+++ client/src/main/java/client/MessageHandler.java
@@ -0,0 +1,5 @@
+package client;
+
+public interface MessageHandler {
+    void onMessage(String message);
+}
---
+++ client/src/main/java/client/WebSocketFacade.java
@@ -0,0 +1,37 @@
+package client;
+
+import com.google.gson.Gson;
+import jakarta.websocket.ClientEndpoint;
+import jakarta.websocket.*;
+import websocket.commands.UserGameCommand;
+
+import java.net.URI;
+
+@ClientEndpoint
+public class WebSocketFacade {
+    private MessageHandler messageHandler;
+    private Session session;
+
+
+    public WebSocketFacade(String serverUrl, MessageHandler messageHandler) throws Exception {
+        this.messageHandler = messageHandler;
+        var container = ContainerProvider.getWebSocketContainer();
+        session = container.connectToServer(this, URI.create(serverUrl));
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        messageHandler.onMessage(message);
+    }
+
+    public void sendMessage(UserGameCommand command) throws Exception {
+        var json = new Gson().toJson(command);
+        session.getBasicRemote().sendText(json);
+    }
+
+}
