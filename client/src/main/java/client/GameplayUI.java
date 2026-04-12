1cafe7422b297d932f8751b0cf967d24fb31bdb1
William <wormy784@byu.edu>
2026-04-11 22:59:37 -0600

    Move and resign and leave done.

--- client/src/main/java/client/GameplayUI.java
+++ client/src/main/java/client/GameplayUI.java
@@ -1,8 +1,12 @@
 package client;
 
+import chess.ChessMove;
+import chess.ChessPosition;
 import model.AuthData;
 import model.GameData;
 import ui.BoardDrawer;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.UserGameCommand;
 
 public class GameplayUI implements MessageHandler {
     private WebSocketFacade command;
@@ -30,9 +34,13 @@ public class GameplayUI implements MessageHandler {
         switch (parts[0]) {
             case "help" -> help();
             case "redraw" -> redraw();
-            case "move" -> move();
+            case "move" -> move((parts[1]), parts[2]);
+
             case "resign" -> resign();
-            case "leave" -> leave();
+            case "leave" -> {
+                leave();
+            return true;
+            }
             case "highlight" -> highlight();
             default -> help();
         }
@@ -41,13 +49,51 @@ public class GameplayUI implements MessageHandler {
 
     private void help() {
         System.out.println("redraw");
-        System.out.println("move <LOCATION>");
+        System.out.println("move <LOCATION> <DESTINATION>");
         System.out.println("resign");
         System.out.println("leave");
         System.out.println("highlight <PIECE>");
     }
 
     private void redraw() {
+        if (currentGame == null) {
+            System.out.println("No game loaded.");
+            return;
+
+        }
+        BoardDrawer drawn = new  BoardDrawer();
+        drawn.drawBoard(currentGame.game().getBoard(), "WHITE".equals(teamColor));
+    }
+
+    private void leave() {
+        try {
+            command.sendMessage(new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void move(String location, String destination) {
+        try {
+            var newLocation = new ChessPosition(Character.getNumericValue(location.charAt(1)),location.charAt(0) - 'a' + 1);
+            var newDestination= new ChessPosition(Character.getNumericValue(destination.charAt(1)),destination.charAt(0) - 'a' + 1);
+            var move = new ChessMove(newLocation, newDestination, null);
+            var moveCommand = new MakeMoveCommand(UserGameCommand.CommandType.MAKE_MOVE, authToken, gameID, move);
+            command.sendMessage(moveCommand);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void resign() {
+        try {
+            command.sendMessage(new UserGameCommand(UserGameCommand.CommandType.RESIGN, authToken, gameID));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void highlight() {
 
     }
 
